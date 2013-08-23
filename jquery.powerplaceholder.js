a4614d03a4778406c0979cbb3d32bcafbba29d01
sujiantao <jiantaosu9@hotmail.com>
2013-08-23 17:34:18 +0800

    add options setting
    add options setting

--- jquery.powerplaceholder.js
+++ jquery.powerplaceholder.js
@@ -4,12 +4,9 @@
 
 (function ($) {
 	"use strict";
-	var defaults = {
-		initOpacity: 0.4,
-		focusOpacityDiff: 0.3,
+	var settings = {
 		overlayedClass: "jq-field-overlayed",
 		overlayClass: "jq-field-overlay",
-		actualOverlayClass: "jq-overlay-text",
 		directions: ["left", "right", "top", "bottom"]
 	};
 	function copyStyles(oriEle, newEle, styles) {
@@ -18,7 +15,7 @@
 			try {
 				if (/\*/.test(style)) {
 					for (var l = 0; 4 > l; l++) {
-						var o = style.replace(/\*/g, defaults.directions[l]), p = oriEle.css(o);
+						var o = style.replace(/\*/g, settings.directions[l]), p = oriEle.css(o);
 						newEle.css(o, p);
 					}
 				} else {
@@ -30,19 +27,26 @@
 		}
 	}
 
-	function setOverlay(input, isEmptyFunc, isFocus) {
+	function hideHelpOverlay(input) {
+		input.css("opacity", 1);
+		input.css("filter", "");
+		input.removeClass(settings.overlayedClass);
+	}
+
+	function setOverlay(input, ops, isEmptyFunc, isFocus) {
 		var overlay, overlaystr, width, height, topdif, overlaytop, leftdif, overlayleft, h;
 		if (input.is(":visible")) {
-			h = $.browser.msie ? 0 : defaults.initOpacity;
+			input.prop('autocomplete', 'off');
+			h = $.browser.msie ? 0.2 : ops.initOpacity;
 			if (isEmptyFunc()) {
-				input.css("opacity", h + (isFocus ? defaults.focusOpacityDiff : 0));
-				input.addClass(defaults.overlayedClass);
-				overlay = input.prev("." + defaults.actualOverlayClass);
+				input.css("opacity", h + (isFocus ? ops.focusOpacityDiff : 0));
+				input.addClass(settings.overlayedClass);
+				overlay = input.prev("." + settings.overlayClass);
 				if (0 === overlay.length) {
-					overlaystr = input.next("." + defaults.actualOverlayClass).text();
+					overlaystr = input.next("." + ops.overlayTextClass).text();
 					width = input.width();
 					height = input.height();
-					overlay = input.clone().prop("className", defaults.actualOverlayClass).attr("name", null).attr("id", null).attr("disabled", "disabled").css({ position: "absolute", backgroundColor: "white", color: "black", "-webkit-text-fill-color": "black", opacity: 1, width: width, height: height });
+					overlay = input.clone().prop("className", settings.overlayClass).attr("name", null).attr("id", null).attr("disabled", "disabled").css({ position: "absolute", backgroundColor: "white", color: "black", "-webkit-text-fill-color": "black", opacity: 1, width: width, height: height });
 					if (overlay.is("textarea,input")) {
 						overlay.val(overlaystr);
 						copyStyles(input, overlay, ["line-height"]);
@@ -69,35 +73,30 @@
 					}
 				}
 			} else {
-				input.css("opacity", 1).css("filter", "").removeClass(defaults.overlayedClass);
+				input.css("opacity", 1).css("filter", "").removeClass(settings.overlayedClass);
 			}
 		}
 	}
 
-	var methods = {
-		genericBindOverlayEvents: function (input, isEmptyFunc) {
-			input.bind("keydown contextmenu", function () {
-				methods.hideHelpOverlay(input);
-			}).focus(function () {
-				setOverlay(input, isEmptyFunc, true);
-			}).blur(function () {
-				setOverlay(input, isEmptyFunc);
-			}).each(function () {
-				setOverlay(input, isEmptyFunc);
-			});
-		},
-		hideHelpOverlay: function (input) {
-			input.css("opacity", 1);
-			input.css("filter", "");
-			input.removeClass(defaults.overlayedClass);
-		}
-	};
-
-	$.fn.powerPlaceholder = function () {
+	$.fn.powerPlaceholder = function (options) {
+		var ops = $.extend({}, $.fn.powerPlaceholder.defaults, options);
 		return this.each(function () {
 			var item = $(this);
-			methods.genericBindOverlayEvents(item, function () { return "" === item.val(); });
+			item.bind("keydown contextmenu", function () {
+				hideHelpOverlay(item);
+			}).focus(function () {
+				setOverlay(item, ops, function () { return "" === item.val(); }, true);
+			}).blur(function () {
+				setOverlay(item, ops, function () { return "" === item.val(); });
+			}).each(function () {
+				setOverlay(item, ops, function () { return "" === item.val(); });
+			});
 		});
 	};
 
+	$.fn.powerPlaceholder.defaults = {
+		initOpacity: 0.4,
+		focusOpacityDiff: 0.2,
+		overlayTextClass: "jq-overlay-text"
+	};
 })(jQuery);
